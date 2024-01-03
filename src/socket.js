c613799ada7780f41ad8c9db4fc7c01f0f868943
lakshay deep <deep.lakshya1310@gmail.com>
2024-01-04 03:41:27 +0530

    url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+const URL = process.env.NODE_ENV === 'production' ?
+  "https://draw-io-server-z8lh.onrender.com" :
+  "http://localhost:5000"
+
+export const socket = io(URL);
 
 
